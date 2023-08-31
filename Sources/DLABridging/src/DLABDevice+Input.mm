e97940a2abdcf8703a260d31293e200b43553ec7
Hart Woolery <gohart@gmail.com>
2023-08-31 12:43:17 -0700

    Update DLABDevice+Input.mm

--- Sources/DLABridging/src/DLABDevice+Input.mm
+++ Sources/DLABridging/src/DLABDevice+Input.mm
@@ -317,7 +317,7 @@ NS_INLINE BOOL copyPlaneDLtoCV(IDeckLinkVideoInputFrame* videoFrame, CVPixelBuff
         pbAttributes[pixelFormatKey] = @(cvPixelFormat);
         pbAttributes[widthKey] = @(videoFrame->GetWidth());
         pbAttributes[heightKey] = @(videoFrame->GetHeight());
-        pbAttributes[surfaceKey] = @(1);
+        pbAttributes[surfaceKey] = @([NSNull null]);
         pbAttributes[bytesPerRowAlignmentKey] = @(16); // = 2^4 = 2 * sizeof(void*)
         
         CVReturn err = kCVReturnError;
